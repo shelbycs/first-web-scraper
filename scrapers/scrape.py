e8a3415ddffa16682364e851f692c98a2e9794a3
shelbycs <ssabat@gwmail.gwu.edu>
2017-04-03 21:38:30 -0400

    adding sports hw

--- scrapers/scrape.py
+++ scrapers/scrape.py
@@ -1,24 +1,30 @@
 import csv
 import requests
 from BeautifulSoup import BeautifulSoup
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
-years = ['2015-2016', '2014-2015', '2013-2014', '2012-2013', '2011-2012', '2010-2011']
+months = ['01', '02', '03', '04']
 list_of_rows = []
 
-for year in years: 
-	print year
-	response = requests.get("https://columbian.gwu.edu/" + year)
-	html = response.content
+for month in months: 
+    print month
+    response = requests.get("http://m.nationals.mlb.com/roster/transactions/2017/" + month)
+    html = response.content
 
-	soup = BeautifulSoup(html)
-	table = soup.find('table')
+    soup = BeautifulSoup(html)
+    table = soup.find('table')
 
-	for row in table.findAll('tr')[1:-1]:
-		list_of_cells = []
-		list_of_cells.append(year)
-    	for cell in row.findAll('td'):
-    		list_of_cells.append(cell.text.encode('utf-8'))
-    	list_of_rows.append(list_of_cells)
+    for row in table.findAll('tr')[1:-1]:
+        list_of_cells = []
+        list_of_cells.append(month)
+        for cell in row.findAll('td'):
+            list_of_cells.append(cell.text.encode('utf-8'))
+        list_of_rows.append(list_of_cells)
+        for cell in row.findAll('a href'):
+        	list_of_cells.append(cell.text.encode('utf-8'))
+        list_of_rows.append(list_of_cells)
 
 outfile = open("grants1.csv", "wb")
 writer = csv.writer(outfile)
